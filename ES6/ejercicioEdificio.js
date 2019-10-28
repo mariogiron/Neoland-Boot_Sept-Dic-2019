cd6e60a1c4e61e8143a46377a1b2a307febfb555
Mario Giron Martin <mariogironm@gmail.com>
2019-10-28 13:01:35 +0100

    Ejercicio Edificio Completo

--- ES6/ejercicioEdificio.js
+++ ES6/ejercicioEdificio.js
@@ -30,7 +30,21 @@ class Vivienda {
 }
 
 class Edificio {
-
+    constructor(pCalle, pNumero) {
+        this.calle = pCalle;
+        this.numero = pNumero;
+        this.casas = [];
+    }
+    agregarCasa(pCasa) {
+        this.casas.push(pCasa);
+    }
+    mostrar() {
+        let resultado = `EDIFICIO. Calle ${this.calle}. Número ${this.numero}\n`;
+        for (let casa of this.casas) {
+            resultado += casa.mostrar();
+        }
+        return resultado;
+    }
 }
 
 let pers1 = new Persona('Roberto', 'Rodríguez', 33);
@@ -40,4 +54,12 @@ let pers3 = new Persona('Alvaro', 'Martín', 56);
 let viv1 = new Vivienda(3, 'D');
 viv1.agregarHabitante(pers1);
 viv1.agregarHabitante(pers2);
-console.log(viv1.mostrar());
+
+let viv2 = new Vivienda(2, 'I');
+viv2.agregarHabitante(pers3);
+
+let edif = new Edificio('Gran Vía', 23);
+edif.agregarCasa(viv1);
+edif.agregarCasa(viv2);
+
+console.log(edif.mostrar());
