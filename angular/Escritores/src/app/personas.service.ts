f91cdc33d2d5a4ede284d46bd331380fcd58b875
Mario Giron Martin <mariogironm@gmail.com>
2019-11-06 17:54:16 +0100

    Ejercicio Escritores - Servicios y LocalStorage

--- angular/Escritores/src/app/personas.service.ts
+++ angular/Escritores/src/app/personas.service.ts
@@ -8,17 +8,48 @@ export class PersonasService {
   arrPersonas: any[];
 
   constructor() {
-    this.arrPersonas = [
-      { id: 1, nombre: 'Mario', apellidos: 'Girón', edad: 35 },
-      { id: 2, nombre: 'Rosa', apellidos: 'López', edad: 46 },
-      { id: 3, nombre: 'Fermín', apellidos: 'Cacho', edad: 25 },
-      { id: 4, nombre: 'Nuria', apellidos: 'Bernudez', edad: 57 },
-      { id: 5, nombre: 'Luis', apellidos: 'Roldán', edad: 42 },
-    ];
+    if (localStorage.getItem('personas')) {
+      this.arrPersonas = JSON.parse(localStorage.getItem('personas'));
+    } else {
+      this.arrPersonas = [
+        { id: 1, nombre: 'Mario', apellidos: 'Girón', edad: 35 },
+        { id: 2, nombre: 'Rosa', apellidos: 'López', edad: 46 },
+        { id: 3, nombre: 'Fermín', apellidos: 'Cacho', edad: 25 },
+        { id: 4, nombre: 'Nuria', apellidos: 'Bernudez', edad: 57 },
+        { id: 5, nombre: 'Luis', apellidos: 'Roldán', edad: 42 },
+      ];
+    }
   }
 
+  // Método que recupera todas las personas
   getAll() {
     return this.arrPersonas;
+  }
+
+  // Método para recuperar las personas mayores de pEdad
+  mayoresDe(pEdad) {
+    // return this.arrPersonas.filter((pers) => {
+    //   return pers.edad > pEdad;
+    // });
+    // return this.arrPersonas.filter(pers => pers.edad > pEdad);
+    const resultado = [];
+    for (const pers of this.arrPersonas) {
+      if (pers.edad > pEdad) {
+        resultado.push(pers);
+      }
+    }
+    return resultado;
+  }
+
+  getById(pId) {
+    return this.arrPersonas.find((pers) => {
+      return pers.id === pId;
+    });
+  }
+
+  insert(pPersona) {
+    this.arrPersonas.push(pPersona);
+    localStorage.setItem('personas', JSON.stringify(this.arrPersonas));
   }
 
 }
--- angular/Escritores/src/app/test/test.component.ts
+++ angular/Escritores/src/app/test/test.component.ts
@@ -1,5 +1,6 @@
 import { PersonasService } from './../personas.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-test',
@@ -9,11 +10,32 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
 
   arr: any[];
+  formPersona: FormGroup;
 
-  constructor(private personasService: PersonasService) { }
+  constructor(private personasService: PersonasService) {
+    this.formPersona = new FormGroup({
+      id: new FormControl(''),
+      nombre: new FormControl(''),
+      apellidos: new FormControl(''),
+      edad: new FormControl('')
+    });
+  }
 
   ngOnInit() {
     this.arr = this.personasService.getAll();
+  }
+
+  manejarClick() {
+    this.arr = this.personasService.mayoresDe(30);
+  }
+
+  mostrarPersona(pId) {
+    const p = this.personasService.getById(pId);
+    alert(`${p.nombre} ${p.apellidos}`);
+  }
+
+  onSubmit() {
+    this.personasService.insert(this.formPersona.value);
   }
 
 }
