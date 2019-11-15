96df448c1cdedc5387179e3e92f488b45ff7dd60
Mario Giron Martin <mariogironm@gmail.com>
2019-11-15 09:55:30 +0100

    Dudas: Ejemplo de Promesas

--- dudas.js
+++ dudas.js
@@ -1,15 +1,37 @@
-let nuevoObj = null;
+// let nuevoObj = null;
 
-let numero = 0;
-try {
-    numero = 4;
-    nuevoObj.toUpperCase();
-} catch (error) {
-    console.log(error.message);
-} finally {
-    console.log('Se ejecuta siempre')
+// let numero = 0;
+// try {
+//     numero = 4;
+//     nuevoObj.toUpperCase();
+// } catch (error) {
+//     console.log(error.message);
+// } finally {
+//     console.log('Se ejecuta siempre')
+// }
+
+// console.log('Hola Mundo');
+// console.log(numero);
+
+function devuelveProm() {
+    let prom = new Promise((resolve, reject) => {
+        if (true) {
+            resolve({ tit: 'mensaje', num: 3, bool: true });
+        } else {
+            reject(12, false);
+        }
+    });
+    return prom;
 }
 
-console.log('Hola Mundo');
-console.log(numero);
+devuelveProm().then((obj) => {
+    console.log(obj);
+}).catch((pNumero, pBoolean) => {
+    console.log(pNumero, pBoolean);
+});
+
+(async () => {
+    await devuelveProm();
+})();
+
 
